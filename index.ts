e8578f4497fd3bbdfab1aa64c1b1a2da92c09ef5
Dev Agrawal <agrawadv@mail.uc.edu>
2024-10-13 23:12:35 -0500

    added subject, async subject, and cleanup

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
+import { createAsync } from "@solidjs/router";
 import { Observable, Subject } from "rxjs";
+import { createMemo, createSignal, onCleanup } from "solid-js";
 
 export type Handler<E> = (<O>(transform: (e: E) => O) => Handler<O>) & {
   $: Observable<E>;
@@ -8,7 +10,8 @@ export type Emitter<E> = (e: E) => void;
 function makeHandler<E>($: Observable<E>): Handler<E> {
   function handler<O>(transform: (e: E) => O): Handler<O> {
     const next$ = new Subject<O>();
-    $.subscribe((e) => next$.next(transform(e)));
+    const sub = $.subscribe((e) => next$.next(transform(e)));
+    onCleanup(() => sub.unsubscribe());
     return makeHandler<O>(next$);
   }
 
@@ -16,7 +19,25 @@ function makeHandler<E>($: Observable<E>): Handler<E> {
   return handler;
 }
 
-export function createEvent<E = never>(): [Handler<E>, Emitter<E>] {
+export function createEvent<E = any>(): [Handler<E>, Emitter<E>] {
   const $ = new Subject<E>();
   return [makeHandler($), (e) => $.next(e)] as const;
+}
+
+export function createSubject<T>(
+  init: T | undefined,
+  ...events: Array<Handler<T | ((prev: T) => T)>>
+) {
+  const [signal, setSignal] = createSignal(init);
+  events.forEach((h) => h(setSignal));
+  return signal;
+}
+
+export function createAsyncSubject<T>(
+  source: () => Promise<T>,
+  ...events: Array<Handler<T | ((prev: T) => T)>>
+) {
+  const asyncSignal = createAsync(source);
+  const subject = createMemo(() => createSubject(asyncSignal(), ...events));
+  return () => subject()();
 }
